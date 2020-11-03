1e4eb2f2629c93e1af6fecf594be8c56f5941b98
Admin <Admin@DESKTOP-7BLC55C>
2020-11-03 15:20:03 +0100

    Nuova classe di prova

---
+++ src/j0072/HelloWorldAugusto.java
@@ -0,0 +1,12 @@
+package j0072;
+
+import java.lang.Object;
+
+public class HelloWorldAugusto {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world Augusto");
+		
+	}	
+
+}
