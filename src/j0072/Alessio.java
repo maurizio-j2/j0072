6cc3b21eafda8f08d3addec2a25c38ff452efacc
ale <ale@192.168.1.51>
2020-11-03 14:20:18 +0100

    Creo la classe Alessio che definisce la mia eta e la richiamo in HelloAlessio;

---
+++ src/j0072/Alessio.java
@@ -0,0 +1,6 @@
+package j0072;
+
+public class Alessio {
+ public	static int anni;
+
+}
---
+++ src/j0072/HelloAlessio.java
@@ -0,0 +1,13 @@
+package j0072;
+import j0072.Alessio;
+public class HelloAlessio {
+
+	public  static void main(String[] args) {
+		Alessio Mestesso = new Alessio ();
+		Mestesso.anni = 29;
+		System.out.println("Hello Classe! ");
+		System.out.println ("La mia eta' e'");
+		System.out.print(Mestesso.anni);
+	}
+
+}
