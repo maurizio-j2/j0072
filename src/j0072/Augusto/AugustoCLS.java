dd2d8b58cffcce26659d89793c1bb18988cc31a1
Maurizio <maurizio.ignizio@gmail.com>
2020-11-09 19:51:29 +0100

    Aggiornamento AugustoCLS

--- src/j0072/Augusto/AugustoCLS.java
+++ src/j0072/Augusto/AugustoCLS.java
@@ -1,5 +1,13 @@
 package j0072.Augusto;
 
 public class AugustoCLS {
+	
+	public static void main(String[] args) {
+		Data DataOdierna = new Data(5,11,2020);
 
+		// Stampo la data
+		System.out.println("Oggi è il giorno: " + DataOdierna.giorno);
+		System.out.println("dell' " + DataOdierna.mese + " esimo mese");
+		System.out.println("dell'anno " + DataOdierna.anno);
+	}
 }
---
+++ src/j0072/Augusto/Data.java
@@ -0,0 +1,14 @@
+package j0072.Augusto;
+
+public class Data {
+	public int giorno;
+	public int mese;
+	public int anno;
+
+// metodo costruttore per la data
+	public Data (int g, int m, int a) {
+		giorno = g;
+		mese = m;
+		anno = a;
+	}
+}
