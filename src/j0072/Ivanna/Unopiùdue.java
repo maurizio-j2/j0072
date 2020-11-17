305aa1525e36d16cc8d61c2dde7d7d1280683d82
ivanna <ivanna@ignizio.com>
2020-11-17 15:32:28 +0100

    numeretto

---
+++ src/j0072/Ivanna/Unopiùdue.java
@@ -0,0 +1,20 @@
+package j0072.Ivanna;
+
+import java.util.Scanner;
+
+public class Unopiùdue {
+
+	public static void main(String[] args) {
+		Scanner numeretto = new Scanner(System.in);
+				int first, second, third, result;
+				System.out.println("Enter the first number: ");
+				first = numeretto.nextInt();
+				System.out.println("Enter the second number: ");
+				second = numeretto.nextInt();
+				System.out.println("Enter the third number: ");
+				third = numeretto.nextInt();
+				result = first + second + third;
+				System.out.println("The result is: " + result);
+				
+			}
+		}
