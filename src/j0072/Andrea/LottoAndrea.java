34422e86ed6525a46e20a27b4f1110c914360a42
andrea <andrea@ignzio.com>
2020-11-19 17:24:33 +0100

    Prova del lotto andrea

---
+++ src/j0072/Andrea/LottoAndrea.java
@@ -0,0 +1,31 @@
+package j0072.Andrea;
+
+
+public class LottoAndrea {
+
+    
+	
+	public String[][] DammiArrayRuota ()  {
+		String[][] Ruota = {  
+				{"napoli", "roma", "milano"},
+				{"25, 36, 2, 12, 22", "10, 34, 43, 24, 76", "44, 14, 11, 76, 87"},
+		};
+		return Ruota;   
+		
+    }
+	
+}
+
+	
+				
+			
+		
+
+
+	
+	
+	
+	
+		
+		
+	
