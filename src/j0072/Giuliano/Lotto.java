d77d88a0f28033306f9a77f380d944d30aa8b785
giuliano <giuliano@ignizio.com>
2020-11-24 17:44:47 +0100

    Creazione classe Lotto

---
+++ src/j0072/Giuliano/Lotto.java
@@ -0,0 +1,16 @@
+package j0072.Giuliano;
+
+public class Lotto {
+	
+		public String[][]ArrayLotto() {
+			String[][] estrazione = {
+				
+				{"Milano: ", "Roma: ", "Napoli: "},
+				{"25, 45, 21, 67, 2", "1, 78, 90, 23, 69", "15, 4, 38, 59, 66"},
+				
+			};
+			return estrazione;
+		}
+
+	
+}
