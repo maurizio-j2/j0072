0c86145a684d13fa8f7a041212cb9b820b14baa6
giuliano <giuliano@ignizio.com>
2020-11-26 16:43:01 +0100

    Lotto

---
+++ src/j0072/Giuliano/Ruota.java
@@ -0,0 +1,12 @@
+package j0072.Giuliano;
+
+public class Ruota {
+	public String citta;
+	public byte[] numero;
+	public Ruota(String ruota_di) {
+		citta = ruota_di;
+		numero = new byte[5];
+	}
+}
+
+
